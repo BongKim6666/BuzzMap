48a7e81b09cb66aa86d96dafd1451e657a6c135a
Bong Kim <bong@BongnoMacBook-Air.local>
2023-10-11 13:12:44 +0900

    Commit message

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ if (isset($_COOKIE['userId'])) {
     <title>Buzz Map </title>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4My62EvC7_WneZqIJ54PBKoi6dpIpuAw&libraries=places&callback=initMap"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4My62EvC7_WneZqIJ54PBKoi6dpIpuAw&libraries=places&callback=initMap&v=weekly"
         defer></script>
     <link rel="stylesheet" href="./css/style.css" />
     <link rel="stylesheet" href="./css/menu.css" />
@@ -29,7 +29,8 @@ if (isset($_COOKIE['userId'])) {
 
 </head>
 
-<body>
+
+<body onload="autoGetLocation()">
 
     <div id="navArea">
         <nav>
@@ -98,6 +99,14 @@ if (isset($_COOKIE['userId'])) {
     <input type="text" id="addressInput" placeholder="住所を入力">
 
     <div id="map"></div>
+
+    <button id="locationButton"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M18 16.016c1.245.529 2 1.223 2 1.984 0 1.657-3.582 3-8 3s-8-1.343-8-3c0-.76.755-1.456 2-1.984" />
+            <path fill="currentColor" fill-rule="evenodd"
+                d="M11.262 17.675 12 17l-.738.675zm1.476 0 .005-.005.012-.014.045-.05.166-.186a38.19 38.19 0 0 0 2.348-2.957c.642-.9 1.3-1.92 1.801-2.933.49-.99.885-2.079.885-3.086C18 4.871 15.382 2 12 2S6 4.87 6 8.444c0 1.007.395 2.096.885 3.086.501 1.013 1.16 2.033 1.8 2.933a38.153 38.153 0 0 0 2.515 3.143l.045.05.012.014.005.005a1 1 0 0 0 1.476 0zM12 17l.738.674L12 17zm0-11a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"
+                clip-rule="evenodd" />
+        </svg></button>
 
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
-var map;
-var marker; // 将 marker 对象提升到外部作用域
+let map;
+let marker;
 
 function initMap() {
   const myLatLng = {
@@ -68,4 +68,68 @@ function initMap() {
       );
     });
   });
+  infoWindow = new google.maps.InfoWindow();
+  console.log(infoWindow);
+
+  const locationButton = document.getElementById("locationButton");
+
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          map.zoom = 18;
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("現在位置です！");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
 }
+function autoGetLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        map.zoom = 18;
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("現在位置です！");
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+window.initMap = initMap;
